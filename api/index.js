352c9f175d38631f3ff02ced6c9033937d5a3c3f
Holat <105239299+Holat@users.noreply.github.com>
2023-09-16 21:02:52 +0100

    packages

--- api/index.js
+++ api/index.js
@@ -3,10 +3,32 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
 app.use("/api/posts", postRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
